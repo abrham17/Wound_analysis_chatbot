32c5832e683c5cf8baa201afcc0606209808cd1d
abrham17 <abrhamhabtom17@gmail.com>
2025-02-12 11:11:05 +0300

    uploaded the model to hugging face

--- backend/health_assistant/api/views.py
+++ backend/health_assistant/api/views.py
@@ -2,17 +2,40 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from PIL import Image
 import torch
+from torchvision import models
 import torch.nn as nn
 from torchvision import transforms, models
 import google.generativeai as genai
 import json
 import os
+from huggingface_hub import hf_hub_download
 
+# Download the model
+from torchvision import models
+
+class CustomResNet(nn.Module):
+    def __init__(self, num_classes=10):  # Adjust num_classes as needed
+        super(CustomResNet, self).__init__()
+        self.base_model = models.resnet50(pretrained=False)
+        self.base_model.fc = nn.Linear(self.base_model.fc.in_features, num_classes)
+    
+    def forward(self, x):
+        return self.base_model(x)
+
+model_path = hf_hub_download(repo_id="Abrhame/resnet-image-classifier-v2", filename="pytorch_model.bin")
+
+# Load the model
+model = CustomResNet(num_classes=7)
+model.load_state_dict(torch.load(model_path) , strict=False)
+model.eval()
 # Load Google Gemini API key
 GOOGLE_API_KEY = "AIzaSyBnIUvvrrMPMDUshQNTBfEEZLzhPtiggTA"  # Replace with your actual API key
 genai.configure(api_key=GOOGLE_API_KEY)
+class_names = ["Abrasion", "Bruises", "Burn", "Cut", "Fracture", "Ingrown_nails", "Laceration"]
 
 # Load ResNet model for image classification
+
+"""
 model = models.resnet50(pretrained=False)
 num_classes = 7
 model.fc = nn.Linear(model.fc.in_features, num_classes)
@@ -24,8 +47,7 @@ model.load_state_dict(torch.load(
 ))
 model.eval()
 
-class_names = ["Abrasion", "Bruises", "Burn", "Cut", "Fracture", "Ingrown_nails", "Laceration"]
-
+"""
 # Image transformations
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
